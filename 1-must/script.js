23ecd4dec865c1f74e83b96f5d3cdbcebb5b216b
soonhee0 <kyoujunki@hotmail.com>
2023-10-06 15:11:15 +0900

    テスト１に解答しました

--- 1-must/script.js
+++ 1-must/script.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 
 /**
  * 【問題1】
- * 
+ *
  * 関数isEvenを定義しましょう。
  * isEvenは、偶数ならtrueを、奇数ならfalseを返します。
- * 
+ *
  * @param {number}
  * @returns {boolean}
  */
 
 // ここに関数を定義しましょう
-
+// ここに関数を定義しましょう
 
 // ここからはテストなので変更を加えないでください
 console.log("*** 問題1 ***");
@@ -21,24 +21,19 @@ test(isEven(5), false);
 test(isEven(3), false);
 test(isEven(10), true);
 
-
-
 ////////////////////////
-
-
 
 /**
  * 【問題2】
- * 
+ *
  * 関数makeStrを定義しましょう。
  * makeStrは、配列の要素をすべて合わせた文字列を返します。
- * 
+ *
  * @param {Array<string>}
  * @returns {string}
  */
 
 // ここに関数を定義しましょう
-
 
 // ここからはテストなので変更を加えないでください
 console.log("*** 問題2 ***");
@@ -47,18 +42,14 @@ test(makeStr(["a", "b", "c"]), "abc");
 test(makeStr(["1", "2", "3"]), "123");
 test(makeStr(["true", " ", "false"]), "true false");
 
-
-
 ////////////////////////
-
-
 
 /**
  * 【問題3】
- * 
+ *
  * 関数pickを定義しましょう。
  * pickは、入れ子になった配列とindex番号を取り、各配列のindexに格納されている値を入れた配列を返します。
- * 
+ *
  * @param {Array<string>}
  * @param {number}
  * @returns {Array<string>}
@@ -66,29 +57,26 @@ test(makeStr(["true", " ", "false"]), "true false");
 
 // ここに関数を定義しましょう
 
-
 // ここからはテストなので変更を加えないでください
 console.log("*** 問題3 ***");
 
 let testArray = [
   ["a", "b", "c"],
   ["d", "e", "f"],
-  ["g", "h", "i"]
+  ["g", "h", "i"],
 ];
 
 test(pick(testArray, 1), ["b", "e", "h"]);
-
 
 testArray = [
   ["m", "s"],
   ["e", "n"],
   ["g", "i"],
   ["n", "e"],
-  ["e", "r"]
+  ["e", "r"],
 ];
 
 test(pick(testArray, 0), ["m", "e", "g", "n", "e"]);
-
 
 testArray = [
   ["1", "2", "3", "4", "5", "6"],
@@ -97,18 +85,14 @@ testArray = [
 
 test(pick(testArray, 4), ["5", "five"]);
 
-
-
 ////////////////////////
-
-
 
 /**
  * 【問題4】
- * 
+ *
  * 関数collectKeysを定義しましょう。
  * collectKeysは第二引数に入った値をvalueに持つkeyを集めた配列を返します。
- * 
+ *
  * @param {object}
  * @param {any}
  * @returns {Array<string>}
@@ -116,53 +100,46 @@ test(pick(testArray, 4), ["5", "five"]);
 
 // ここに関数を定義しましょう
 
-
 // ここからはテストなので変更を加えないでください
 console.log("*** 問題4 ***");
 
 let testObject = {
-  2 : "winter",
-  7 : "summer",
-  4 : "spring",
-  8 : "summer",
-  11 : "autumn",
-}
+  2: "winter",
+  7: "summer",
+  4: "spring",
+  8: "summer",
+  11: "autumn",
+};
 
 test(collectKeys(testObject, "summer"), ["7", "8"]);
 
-
 testObject = {
-  a : true,
-  b : "user",
-  c : 2,
-  d : "member",
-  e : true,
-  h : 77
-}
+  a: true,
+  b: "user",
+  c: 2,
+  d: "member",
+  e: true,
+  h: 77,
+};
 
 test(collectKeys(testObject, true), ["a", "e"]);
 
-
 testObject = {
-  a : 10,
-  b : 100,
-  c : 15,
-}
+  a: 10,
+  b: 100,
+  c: 15,
+};
 
 test(collectKeys(testObject, 100), ["b"]);
 
-
-
 ////////////////////////
-
-
 
 /**
  * 【問題5】
- * 
+ *
  * 関数createObjectを定義しましょう。
  * createObjectは、第一引数に入った配列の値をkeyに、第二引数に入った配列の値をvalueに持つオブジェクトを返します。
- * 
+ *
  * @param {Array<string>}
  * @param {Array<any>}
  * @returns {object<any>}
@@ -170,14 +147,15 @@ test(collectKeys(testObject, 100), ["b"]);
 
 // ここに関数を定義しましょう
 
-
 // ここからはテストなので変更を加えないでください
 console.log("*** 問題5 ***");
 
-test(createObject(["a", "b", "c"], [1, 2, 3]), {a: 1, b: 2, c: 3});
-test(createObject(["userId", "userName", "isMember"], [100, "Hitomi", true]), {userId: 100, userName: "Hitomi", isMember: true});
+test(createObject(["a", "b", "c"], [1, 2, 3]), { a: 1, b: 2, c: 3 });
+test(createObject(["userId", "userName", "isMember"], [100, "Hitomi", true]), {
+  userId: 100,
+  userName: "Hitomi",
+  isMember: true,
+});
 test(createObject([], []), {});
-
-
 
 ////////////////////////
